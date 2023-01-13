e647b6c3c8b25d23dfa67516f7678f69a2b0b3a0
Gabriel Alves dos Santos <gabrielalvesdossantos16@gmail.com>
2023-01-13 09:28:04 -0300

    Suporte para imagem, cadastrar usuario, index

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,40 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Header from './../components/header';
+import axios from 'axios';
+import Produtos from './routes/produtos';
 
-const inter = Inter({ subsets: ['latin'] })
+export const getServerSideProps = async (context) => {
+  const response = await axios.get("http://localhost:3002" + "/produtos");
+  const attributes = await response.data;
+  return {
+    props: {
+      attributes
+    },
+  };
+};
 
-export default function Home() {
+
+export default function Home({attributes}) {
+  const produtos = attributes.slice(0,8)
   return (
    <div>
     <Header />
     <h3>Ultimos Produtos</h3>
+    <div className='produtosindex'>
+    {produtos.map(({id,nome, desc, img, preco})=>(
+      <div key={id}>
+        <div className='card'>
+        <Image src={`http://localhost:3002/produto/img/${id}`} width={200} height={200} className="imagemcard"/>
+        <div className='nomeproduto'>{nome}</div>
+        <div className='infoproduto'>
+        <div className='precoproduto'>R${preco}</div>
+        <button className='botaoproduto'>Ir ao Produtos</button>
+        </div>
+        </div>
+      </div>
+    ))}
+    </div>
    </div>
   )
 }
--- pages/routes/admin/cadastrar/produto.js
+++ pages/routes/admin/cadastrar/produto.js
@@ -1,4 +1,140 @@
+import Header from "../../../../components/header";
+import axios from 'axios'
+import {useRouter} from "next/router"
+import { v4 as uuidv4 } from "uuid";
+import {useState} from 'react'
 
 export default function CadastrarProduto(){
-    return 
-}
+   const [file, setFile] = useState();
+    const [produto, setproduto] = useState({
+
+        nome:"",
+        desc:"",
+        img: uuidv4(),
+        preco:"",
+       
+      });
+    
+      let router = useRouter();
+    
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        const emptyFieldCheck = Object.values(produto).some(
+          (element) => element === ""
+        );
+        if (emptyFieldCheck) {
+          toast.error("Há algum campo vazio");
+          return;
+        }
+    
+        const data = {
+          ...produto,
+        };
+        const url = "http://localhost:3002/produto"
+         
+        
+        formData.append("nome", data.nome);
+        formData.append("desc", data.desc);
+        formData.append("img", data.img);
+        formData.append("preco", data.preco);
+        formData.append("imageFile", file);
+        const config = {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        };
+  
+        axios
+          .post(url, formData, config)
+          .then((response) => {
+            console.log(response.data);
+            router.push("/");
+          })
+          .catch((error) => {
+            console.log(error.response);
+          });
+      }
+    
+      
+      
+    
+        const handleInputChange = (e) => {
+          const { id, value } = e.target;
+          setproduto({ ...produto, [id]: value });
+        };
+    
+        const handleFileSelect = (e) => {
+          setFile(e.target.files[0]);
+        };
+    
+      const {nome, desc, img, preco } = produto;
+    return(
+        <div>
+            <Header/>
+            <form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>Cadastro de Produto</legend>
+          <div className="">
+            <span className="" id="basic-addon1">
+              Nome
+            </span>
+            <input
+              id="nome"
+              type="text"
+              onChange={handleInputChange}
+              value={produto.nome}
+              className="form-control"
+              aria-label="Username"
+              aria-describedby="basic-addon1"
+              maxLength={25}
+            />
+          </div>
+          <div className="">
+            <span className="" id="basic-addon1">
+             desc
+            </span>
+            <input
+              id="desc"
+              type="text"
+              onChange={handleInputChange}
+              value={produto.desc}
+              className="form-control"
+              aria-label="Username"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+          <div className="">
+            <span className="" id="basic-addon1">
+              preco
+            </span>
+            <input
+              id="preco"
+              type="number"
+              onChange={handleInputChange}
+              value={produto.preco}
+              className="form-control"
+              aria-label="Username"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+          <div className="">
+            <span className="" id="basic-addon1">
+              Imagem
+            </span>
+            <input
+            name="imageFile"
+              id="imageFile"
+              type="file"
+              onChange={handleFileSelect} required
+              className="form-control"
+            />
+          </div>
+          <button className="" type="submit">
+            Cadastrar
+          </button>
+        </fieldset>
+      </form>
+        </div>
+    )
+    }
